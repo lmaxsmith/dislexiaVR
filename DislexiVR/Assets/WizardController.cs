14957054ee81c25243608941b7983b39a1afe965
vxrlc <48801142+vxrlc@users.noreply.github.com>
2020-01-17 23:32:09 -0500

    BaseScene
    Added base scene setup and wizard start

---
+++ DislexiVR/Assets/WizardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardController : MonoBehaviour
+{
+    public Animator anim;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioSource.isPlaying)
+        {
+            
+        } else
+        {
+            anim.SetBool("Talking", false);
+        }
+    }
+}
