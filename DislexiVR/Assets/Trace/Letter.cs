8ca18be472201b092891de54c04ca59c355242fc
Logan Smith <lmaxsmith@gmail.com>
2020-01-17 22:37:55 -0500

    Instnsiating bulbs by scriptable assets, baby!

--- DislexiVR/Assets/Trace/Letter.cs
+++ DislexiVR/Assets/Trace/Letter.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Letter : MonoBehaviour
 {
+    [Tooltip("Number of strokes required to finish the letter.")]
+    public int strokes = 1;
+    [Tooltip("List of all letter bulbs contained within the letter.")]
+    public LetterBulb[] letterBulbs;
+    public GameObject letterBulbPrefab;
+    public LetterConfiguration letterConfiguration;
+
+    //relationships
+    private void Awake()
+    {
+        letterBulbs = GetComponentsInChildren<LetterBulb>(true);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +28,33 @@ public class Letter : MonoBehaviour
     {
         
     }
+
+    [ContextMenu("Save Letter Configuration")]
+    public void SaveConfiguration()
+    {
+        //setup bulbs
+        letterBulbs = GetComponentsInChildren<LetterBulb>();
+        letterConfiguration.bulbPositions = new Vector3[letterBulbs.Length];
+        for (int i = 0; i < letterBulbs.Length; i++)
+        {
+            letterConfiguration.bulbPositions[i] = letterBulbs[i].transform.localPosition;
+        }
+
+        letterConfiguration.strokes = strokes;
+    }
+
+    [ContextMenu("Load Letter Configuration")]
+    public void LoadConfiguration()
+    {
+        //setup bulbs
+        foreach (var bulbPosition in letterConfiguration.bulbPositions)
+        {
+            GameObject bulbObject = Instantiate(letterBulbPrefab);
+            bulbObject.transform.SetParent(transform);
+            bulbObject.transform.localPosition = bulbPosition;
+        }
+
+        strokes = letterConfiguration.strokes;
+    }
+
 }
---
+++ DislexiVR/Assets/Trace/LetterConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Letter Configuration", menuName = "ScriptableObjects/LetterConfiguration", order = 1)]
+public class LetterConfiguration : ScriptableObject
+{
+    public string letter;
+
+    public int strokes = 1;
+
+    public Vector3[] bulbPositions;
+
+}
